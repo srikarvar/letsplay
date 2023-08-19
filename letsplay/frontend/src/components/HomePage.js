2ed34bf25e27549d06f1fbb17d9452450685425b
srikarcw <45589493+srikarvar@users.noreply.github.com>
2023-08-19 13:20:02 +0530

    user in room api, mounting home page, session mgmt

--- letsplay/frontend/src/components/HomePage.js
+++ letsplay/frontend/src/components/HomePage.js
@@ -1,43 +1,73 @@
+
 import React, { Component } from "react";
-import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
+import RoomJoinPage from "./RoomJoinPage";
 import Room from "./Room";
-import { useNavigate } from "react-router-dom";
-
-import { BrowserRouter as Router,  
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core"
+import {
+    BrowserRouter as Router,
+    Route,
     Routes,
-    Route, 
-    Link, 
-    Redirect } from "react-router-dom";
-
-
-const withRouter = WrappedComponent => props => {
-        const navigate = useNavigate();
-        // etc... other react-router-dom v6 hooks
-      
-        return (
-          <WrappedComponent
-            {...props}
-            navigate={navigate}
-            // etc...
-          />
-        );
-      };
+    Link,
+    useRoutes,
+    Navigate
+} from "react-router-dom";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
-    
-}
+        this.state = {
+            roomCode: null,
+        };
+    }
 
-render() {
-    return ( <Router>
-        <Routes>
-            <Route exact path="/" element={<p>This is the home page</p>}/>
-            <Route path="/join" element={<RoomJoinPage/>}/>
-            <Route path="/create-room" element={<CreateRoomPage/>}/>
-            <Route path="/room/:roomCode" element={<Room/>}/>
-        </Routes>
-    </Router>
-    );
-}}
+    async componentDidMount() {
+        const response = await fetch("/api/user-in-room");
+        const data = await response.json();
+        this.setState({
+          roomCode:data.code
+        })
+    }
+
+
+    renderHomePage() {
+        if(this.state.roomCode) {
+          return(
+            <Navigate to={`/room/${this.state.roomCode}`} replace={true} />
+          )
+        } else {
+          return (
+            <Grid container spacing={3}>
+              <Grid item xs={12} align="center">
+                <Typography variant="h3" compact="h3">
+                  House Party
+                </Typography>
+              </Grid>
+              <Grid item xs={12} align="center">
+                <ButtonGroup disableElevation variant="contained" color="primary">
+                  <Button color="primary" to="/join" component={Link}>
+                    Join a Room
+                  </Button>
+                  <Button color="secondary" to="/create" component={Link}>
+                    Create a Room
+                  </Button>
+                </ButtonGroup>
+              </Grid>
+            </Grid>
+          );
+       }
+    }
+  
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={this.renderHomePage()} />
+                    <Route path="/join" element={<RoomJoinPage />} />
+                    <Route path="/create-room" element={<CreateRoomPage />} />
+                    <Route exact path="/room/:roomCode" element={<Room />} render={({ match }) => <Room id={match.params.roomCode} />} />
+                </Routes>
+            </Router>
+        )
+    }
+}
